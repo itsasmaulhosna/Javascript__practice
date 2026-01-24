8c49fc926c02156f10f6ced62d80522f3d7754e9
itsasmaulhosna <itsasmaulhosna@gmail.com>
2026-01-24 22:13:37 +0600

    first commit

---
+++ practice.js
@@ -0,0 +1,51 @@
+// if (3 < 10) {
+//   console.log('give me a pen');
+// }
+// var apple = 250;
+// if (apple > 300) {
+//   console.log('give me apple');
+// }
+// var weight = 40;
+// if (weight < 20) {
+//   console.log('i will acrry it by myself');
+// } else {
+//   console.log('i will rent a rickshaw');
+// }
+// const salary = 25000;
+// const height = 61;
+// const weight = 60;
+// if (salary > 20000 || height > 66) {
+//   console.log('good');
+// } else {
+//   console.log('bad');
+// }
+// const price = 5000;
+// if (price >= 5000) {
+//   const discount = (price * 10) / 100;
+//   console.log(discount);
+//   const payAmmount = price - discount;
+//   console.log(payAmmount);
+// }
+// const price = 500;
+// const age = 22;
+
+// if (age <= 12) {
+//   console.log('free');
+// } else if (age > 50) {
+//   const discount = (price * 50) / 100;
+//   const payAmmount = price - discount;
+//   console.log(payAmmount);
+// } else {
+//   console.log(price);
+// }
+// const age = 18;
+// age >= 20 ? console.log('vote deo') : console.log('ghumai thako');
+// const price = 500;
+// const isLeader = true;
+// price = isLeader === true ? 0 : price + 100;
+const isPassed = true;
+if (!isPassed) {
+  console.log('biya diya dimu');
+} else {
+  console.log('valo meye');
+}
---
+++ task.js
@@ -0,0 +1,31 @@
+// let burger = 700;
+// if (burger <= 500) {
+//   console.log('Free');
+// } else {
+//   console.log('coke is 30tk');
+// }
+
+// let BMI = 50;
+// if (BMI < 18.5) {
+//   console.log('underweight');
+// } else if (BMI >= 18.5 && BMI <= 24.5) {
+//   console.log('normal');
+// } else if (BMI >= 25 && BMI <= 29.5) {
+//   console.log('overweight');
+// } else {
+//   console.log('obese');
+// }
+
+// marks
+let marks = 50;
+if (marks >= 90 && marks <= 100) {
+  console.log('you got A');
+} else if (marks >= 80 && marks <= 89) {
+  console.log('you got B');
+} else if (marks >= 70 && marks <= 79) {
+  console.log('you got C');
+} else if (marks >= 60 && marks <= 69) {
+  console.log('you got D');
+} else {
+  console.log('you got F');
+}
