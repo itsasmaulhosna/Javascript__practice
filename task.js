9099ac90d7c829c97527a61f387239f58f08b9a0
itsasmaulhosna <itsasmaulhosna@gmail.com>
2026-01-24 22:17:29 +0600

    Update

--- task.js
+++ task.js
@@ -1,20 +1,20 @@
-// let burger = 700;
-// if (burger <= 500) {
-//   console.log('Free');
-// } else {
-//   console.log('coke is 30tk');
-// }
-
-// let BMI = 50;
-// if (BMI < 18.5) {
-//   console.log('underweight');
-// } else if (BMI >= 18.5 && BMI <= 24.5) {
-//   console.log('normal');
-// } else if (BMI >= 25 && BMI <= 29.5) {
-//   console.log('overweight');
-// } else {
-//   console.log('obese');
-// }
+let burger = 700;
+if (burger <= 500) {
+  console.log('Free');
+} else {
+  console.log('coke is 30tk');
+}
+// BMI
+let BMI = 50;
+if (BMI < 18.5) {
+  console.log('underweight');
+} else if (BMI >= 18.5 && BMI <= 24.5) {
+  console.log('normal');
+} else if (BMI >= 25 && BMI <= 29.5) {
+  console.log('overweight');
+} else {
+  console.log('obese');
+}
 
 // marks
 let marks = 50;
