8fd191d10ea41fef38bd6c38a515d6a2dfd7abf0
itsasmaulhosna <itsasmaulhosna@gmail.com>
2026-01-27 22:29:30 +0600

    For loop task2

--- loops/for.js
+++ loops/for.js
@@ -4,3 +4,18 @@ for (let i = 0; i <= 60; i++) {
     'I will invest at least 6 hrs every single day for next 60 days!',
   );
 }
+// 2
+// odd number
+for (let i = 61; i <= 100; i++) {
+  console.log(i);
+  if (i % 2 !== 0) {
+    console.log('odd number', i);
+  }
+}
+// even number
+for (let i = 8; i <= 98; i++) {
+  console.log(i);
+  if (i % 2 !== 1) {
+    console.log('even number', i);
+  }
+}
