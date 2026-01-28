1a65f6e158847be824ecb5585dac8aca55b76c8a
itsasmaulhosna <itsasmaulhosna@gmail.com>
2026-01-28 22:10:49 +0600

    while loop task3

--- loops/while.js
+++ loops/while.js
@@ -1,27 +1,34 @@
 // 1
-let n = 1;
-while (n < 60) {
-  console.log(
-    'I will invest at least 6 hrs every single day for next 60 days!',
-  );
-  n++;
-}
+// let n = 1;
+// while (n < 60) {
+//   console.log(
+//     'I will invest at least 6 hrs every single day for next 60 days!',
+//   );
+//   n++;
+// }
 // 2
-odd number
-let i = 61;
-while (i <= 100) {
-  console.log(i);
-  i++;
-  if (i % 2 !== 0) {
-    console.log('odd number', i);
-  }
-}
+// odd number
+// let i = 61;
+// while (i <= 100) {
+//   console.log(i);
+//   i++;
+//   if (i % 2 !== 0) {
+//     console.log('odd number', i);
+//   }
+// }
 // even number
-let i = 78;
-while (i <= 98) {
-  console.log(i);
+// let i = 78;
+// while (i <= 98) {
+//   console.log(i);
+//   i++;
+//   if (i % 2 === 0) {
+//     console.log('even number', i);
+//   }
+// }
+// table
+let n = 5;
+let i = 1;
+while (i <= 10) {
+  console.log(n * i);
   i++;
-  if (i % 2 === 0) {
-    console.log('even number', i);
-  }
 }
