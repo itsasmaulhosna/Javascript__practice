f55a60a673ec9be05cd77160cec6908eac686817
itsasmaulhosna <itsasmaulhosna@gmail.com>
2026-01-29 21:34:26 +0600

    Continue task2

--- loops/continue.js
+++ loops/continue.js
@@ -1,5 +1,17 @@
+// 1
 for (i = 1; i <= 40; i++) {
   if (i % 2 !== 0) {
+    continue;
+  }
+  console.log(i);
+}
+
+// 2
+for (let i = 55; i <= 85; i++) {
+  if (i % 2 === 0) {
+    continue;
+  }
+  if (i % 5 === 0) {
     continue;
   }
   console.log(i);
