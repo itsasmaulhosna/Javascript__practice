32ff2f1944322b3bd5207ccb6a2450b40037dbb6
itsasmaulhosna <itsasmaulhosna@gmail.com>
2026-01-29 21:49:22 +0600

    Break task2

--- loops/break.js
+++ loops/break.js
@@ -1,6 +1,18 @@
+// 1
 for (let i = 1; i <= 200; i++) {
   if (i === 100) {
     break;
   }
   console.log(i);
+}
+
+// 2
+let i = 1;
+let sum = 0;
+while (sum < 100) {
+  sum += i;
+  if (sum >= 100) {
+    break;
+  }
+  console.log(sum);
 }
