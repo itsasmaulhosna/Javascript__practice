e025d954134de1caa196e2fb42a7dfc5cca41da8
itsasmaulhosna <itsasmaulhosna@gmail.com>
2026-01-30 22:24:47 +0600

    String task2

--- loops/string.js
+++ loops/string.js
@@ -1,8 +1,20 @@
-let string = 'Amdaer deasher name bangladesh';
+// 1
+// let string = 'Amdaer deasher name bangladesh';
+// let count = 0;
+// for (let i = 0; i < string.length; i++) {
+//   let letter = string[i];
+//   if (letter.toLowerCase() === 'a') {
+//     count++;
+//   }
+// }
+// console.log(count);
+
+// 2
+let bd = 'Amader deasher name Bangladesh. Ami amr deshk onekk valobashi.';
 let count = 0;
-for (let i = 0; i < string.length; i++) {
-  let letter = string[i];
-  if (letter.toLowerCase() === 'a') {
+for (let i = 0; i < bd.length; i++) {
+  let letter = bd[i];
+  if (letter === 'A' || letter === 'a') {
     count++;
   }
 }
