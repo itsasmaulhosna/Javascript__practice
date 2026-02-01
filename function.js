21f523211bfb640b5422726bf15772302ad5a72b
itsasmaulhosna <itsasmaulhosna@gmail.com>
2026-02-01 07:17:32 +0600

    Square of function

--- function.js
+++ function.js
@@ -5,3 +5,10 @@ function sum(a, b, c, d, e) {
   console.log(total);
 }
 sum(1, 2, 3, 4, 5);
+
+// 2
+function square(number) {
+  const borgo = number * number;
+  console.log('the square of number is :', borgo);
+}
+square(7);
--- practice.js
+++ practice.js
@@ -230,19 +230,19 @@ let bike = {
 
 // function
 // square
-// function square(number) {
-//   const borgo = number * number;
-//   console.log('the square of number is :', borgo);
-// }
-// square(7);
+function square(number) {
+  const borgo = number * number;
+  console.log('the square of number is :', borgo);
+}
+square(7);
 
 // sum
-function sum(a, b, c, d, e) {
-  console.log(a, b, c, d, e);
-  const total = a + b + c + d + e;
-  console.log(total);
-}
-sum(1, 2, 3, 4, 5);
+// function sum(a, b, c, d, e) {
+//   console.log(a, b, c, d, e);
+//   const total = a + b + c + d + e;
+//   console.log(total);
+// }
+// sum(1, 2, 3, 4, 5);
 
 // return
 function tenTime(number) {
