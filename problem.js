ab4da2d464785fa7f9ff38c73b69af42efcf8f1a
itsasmaulhosna <itsasmaulhosna@gmail.com>
2026-02-02 07:19:11 +0600

    Problem 7

--- problem.js
+++ problem.js
@@ -98,22 +98,22 @@ console.log(ran);
 
 // // 7
 
-// // swap
-// let a = 5;
-// let b = 7;
-// let temp = a;
-// a = b;
-// b = temp;
-// console.log(a, b);
+// swap
+let a = 5;
+let b = 7;
+let temp = a;
+a = b;
+b = temp;
+console.log(a, b);
 
-// let x = 5;
-// let y = 3;
-// console.log(x, y);
-// [x, y] = [y, x];
-// console.log(x, y);
+let x = 5;
+let y = 3;
+console.log(x, y);
+[x, y] = [y, x];
+console.log(x, y);
 
-// for (var i = 0; i < 5; i++) {
-//   setTimeout(function () {
-//     console.log(i);
-//   }, 100);
-// }
+for (var i = 0; i < 5; i++) {
+  setTimeout(function () {
+    console.log(i);
+  }, 100);
+}
